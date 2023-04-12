fdf31eb3f783d5b3126463e138da0a315daf2c4d
Ray Wolt <rjwolt@gmail.com>
2023-04-12 15:48:10 -0500

    Add routes and controllers for category and item

---
+++ controllers/categoryController.js
@@ -0,0 +1,37 @@
+const Category = require("../models/category");
+
+exports.index = (req, res) => {
+  res.send("NOT IMPLEMENTED: site home page");
+};
+
+exports.category_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: category list");
+};
+
+exports.category_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category detail");
+};
+
+exports.category_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category create GET");
+};
+
+exports.category_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category create POST");
+};
+
+exports.category_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category update GET");
+};
+
+exports.category_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category update POST");
+};
+
+exports.category_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category delete GET");
+};
+
+exports.category_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Category delete POST");
+};
---
+++ controllers/itemController.js
@@ -0,0 +1,33 @@
+const Item = require("../models/item");
+
+exports.item_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: item list");
+};
+
+exports.item_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: item detail");
+};
+
+exports.item_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: item create GET");
+};
+
+exports.item_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: item create POST");
+};
+
+exports.item_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: item update GET");
+};
+
+exports.item_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: item update POST");
+};
+
+exports.item_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: item delete GET");
+};
+
+exports.item_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: item delete POST");
+};
