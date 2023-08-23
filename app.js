556926224619d693103c2ed9ebc32a023da2e7c3
Ray Wolt <rjwolt@gmail.com>
2023-08-23 14:52:57 -0500

    Reconfigure multer disk storage for storage volume

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const app = express();
 //Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
+const dev_db_url =
   "mongodb+srv://ray:b8gWjdfB2UrUfjTnYbbMoUuN@cluster0.muml7gw.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -10,7 +10,7 @@ const multer = require("multer");
 // Configure multer disk storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/uploads/");
+    cb(null, "/uploads/images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
