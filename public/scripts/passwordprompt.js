6ef83f9d24cb5f93cfc2813db8b4318fd313a24d
Ray Wolt <rjwolt@gmail.com>
2023-08-22 19:17:46 -0500

    Add custom modal for password prompt

--- public/scripts/passwordprompt.js
+++ public/scripts/passwordprompt.js
@@ -1,9 +1,33 @@
 const passwordProtectedForm = document.getElementById("protected-form");
-const submitButton = document.getElementById("submit-button");
-const url = window.location.href;
+const formSubmitButton = document.getElementById("submit-button");
+const currentUrl = window.location.href;
 
-submitButton.addEventListener("click", async (e) => {
-  const adminPassword = prompt("Please enter the admin password");
+// Create a custom modal for password input
+const modal = document.createElement("div");
+modal.classList.add("modal");
+modal.classList.add("hidden");
+const passwordPrompt = document.createElement("p");
+const promptText = document.createTextNode("Please enter the admin password");
+passwordPrompt.appendChild(promptText);
+const passwordInput = document.createElement("input");
+passwordInput.type = "password";
+const promptSubmitButton = document.createElement("button");
+const buttonText = document.createTextNode("Submit");
+promptSubmitButton.appendChild(buttonText);
+modal.appendChild(passwordPrompt);
+modal.appendChild(passwordInput);
+modal.appendChild(promptSubmitButton);
+document.body.appendChild(modal);
+
+// Show or hide the password prompt on when submitting the form
+formSubmitButton.addEventListener("click", (e) => {
+  modal.classList.toggle("hidden");
+});
+
+// Event listener for the password prompt submit
+promptSubmitButton.addEventListener("click", async (e) => {
+  modal.classList.toggle("hidden");
+  const adminPassword = passwordInput.value;
   const passwordProtectedFormData = new FormData(passwordProtectedForm);
   passwordProtectedFormData.append("adminPassword", adminPassword);
 
@@ -18,7 +42,7 @@ submitButton.addEventListener("click", async (e) => {
       window.location.reload();
     } else {
       xhr.abort();
-      if (url.toString().includes("item")) {
+      if (currentUrl.toString().includes("item")) {
         window.location.href = "/shop/items";
       } else {
         window.location.href = "/shop/categories";
